36a85e197e02c401375c0d15c2853dd77f63e51e
Karin Taliga <karin.taliga@elicit.se>
2021-09-25 11:58:40 +0200

    Add button for milage toggle
    Notice property of component changes onClick, but the changes
    are not showing. We need to explicitly render the changes

--- src/components/VehicleDetail.js
+++ src/components/VehicleDetail.js
@@ -1,22 +1,24 @@
 // 1. Importera React
 import React from "react";
 
-import './vehicledetail.css';
+import "./vehicledetail.css";
 
 // 2. Skapa komponenten
 const VehicleDetail = (props) => {
-	console.log(props);
+	let displayMileage = false;
+
+	const onClick = () => {
+		displayMileage = !displayMileage;
+	}
+
   return (
     <div className="vehicle-item">
-      <img
-        src={props.imageUrl}
-        alt={props.make}
-        className="thumbnail"
-      />
+      <img src={props.imageUrl} alt={props.make} className="thumbnail" />
       <div>{props.registrationNo}</div>
       <div>{props.make}</div>
       <div>{props.model}</div>
       <div>{props.modelYear}</div>
+      <button onClick={onClick} className="btn btn-outline-rounded">{ {displayMileage} ? 'Visa' : 'Göm' } antal km </button>
       <div>{props.mileage}</div>
     </div>
   );
