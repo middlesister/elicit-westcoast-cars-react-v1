152cb03975a79ab1dc397fdc21e28e5a72d54ecc
Karin Taliga <karin.taliga@elicit.se>
2021-09-25 10:41:36 +0200

    Add skeleton (hard-coded) structure for vehicle-item

--- src/App.js
+++ src/App.js
@@ -24,6 +24,50 @@ const App = () => {
           </ul>
         </div>
       </header>
+      <main className="content">
+        <section className="card">
+          <div className="vehicle-item">
+            <img 
+              src="https://i.postimg.cc/N09fbtKZ/car1.jpg"
+              alt=""
+              className="thumbnail" 
+            />
+            <div>ABC123</div>
+            <div>Chevrolet</div>
+            <div>Corvette</div>
+            <div>2019</div>
+            <div>150000</div>
+          </div>
+        </section>
+        <section className="card">
+          <div className="vehicle-item">
+            <img 
+              src="https://i.postimg.cc/N09fbtKZ/car1.jpg"
+              alt=""
+              className="thumbnail" 
+            />
+            <div>ABC123</div>
+            <div>Chevrolet</div>
+            <div>Corvette</div>
+            <div>2019</div>
+            <div>150000</div>
+          </div>
+        </section>
+        <section className="card">
+          <div className="vehicle-item">
+            <img 
+              src="https://i.postimg.cc/N09fbtKZ/car1.jpg"
+              alt=""
+              className="thumbnail" 
+            />
+            <div>ABC123</div>
+            <div>Chevrolet</div>
+            <div>Corvette</div>
+            <div>2019</div>
+            <div>150000</div>
+          </div>
+        </section>
+      </main>
     </section>
   ); // JSX
 };
