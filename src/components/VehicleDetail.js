fbdad8ff87545ee37f4f5a609e0e1e098a1a387b
Karin Taliga <karin.taliga@elicit.se>
2021-09-25 11:00:17 +0200

    Break out component VehicleDetail

---
+++ src/components/VehicleDetail.js
@@ -0,0 +1,27 @@
+// 1. Importera React
+import React from "react";
+
+// 2. Skapa komponenten
+const VehicleDetail = () => {
+  return (
+    <section>
+      <section className="card">
+        <div className="vehicle-item">
+          <img
+            src="https://i.postimg.cc/N09fbtKZ/car1.jpg"
+            alt=""
+            className="thumbnail"
+          />
+          <div>ABC123</div>
+          <div>Chevrolet</div>
+          <div>Corvette</div>
+          <div>2019</div>
+          <div>150000</div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+// 3. Exportera komponenten
+export default VehicleDetail;
--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,52 +1,17 @@
 // 1. Importera React
 import React from "react";
 
+import VehicleDetail from "./VehicleDetail";
+
 // 2. Skapa komponenten
 const Vehicles = () => {
   return (
     <section>
-      <section className="card">
-        <div className="vehicle-item">
-          <img
-            src="https://i.postimg.cc/N09fbtKZ/car1.jpg"
-            alt=""
-            className="thumbnail"
-          />
-          <div>ABC123</div>
-          <div>Chevrolet</div>
-          <div>Corvette</div>
-          <div>2019</div>
-          <div>150000</div>
-        </div>
-      </section>
-      <section className="card">
-        <div className="vehicle-item">
-          <img
-            src="https://i.postimg.cc/N09fbtKZ/car1.jpg"
-            alt=""
-            className="thumbnail"
-          />
-          <div>ABC123</div>
-          <div>Chevrolet</div>
-          <div>Corvette</div>
-          <div>2019</div>
-          <div>150000</div>
-        </div>
-      </section>
-      <section className="card">
-        <div className="vehicle-item">
-          <img
-            src="https://i.postimg.cc/N09fbtKZ/car1.jpg"
-            alt=""
-            className="thumbnail"
-          />
-          <div>ABC123</div>
-          <div>Chevrolet</div>
-          <div>Corvette</div>
-          <div>2019</div>
-          <div>150000</div>
-        </div>
-      </section>
+      <VehicleDetail />
+      <VehicleDetail />
+      <VehicleDetail />
+      <VehicleDetail />
+      <VehicleDetail />
     </section>
   );
 };
