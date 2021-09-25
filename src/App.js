c9c4d2044adbf5280dda35ba639b49c22d3798e8
Karin Taliga <karin.taliga@elicit.se>
2021-09-25 10:39:02 +0200

    Enable prettier code formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 // 1. Importera React ...
-import React from 'react';
+import React from "react";
 
-import './app.css';
+import "./app.css";
 
 // 2. Skapa komponenten
 const App = () => {
-    return (
-        <section>
-            <header>
-                <div className="navbar" id="navbar" >
-                    <h1 className="logo">
-                        <span className="text-primary">Westcoast</span> Cars
-                    </h1>
-                    <ul>
-                        <li><a href="index.html" className="active">Start</a></li>
-                        <li><a href="gallery.html">Galleri</a></li>
-                    </ul>
-                </div>
-            </header>
-        </section>
-    ); // JSX
-}
+  return (
+    <section>
+      <header>
+        <div className="navbar" id="navbar">
+          <h1 className="logo">
+            <span className="text-primary">Westcoast</span> Cars
+          </h1>
+          <ul>
+            <li>
+              <a href="index.html" className="active">
+                Start
+              </a>
+            </li>
+            <li>
+              <a href="gallery.html">Galleri</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </section>
+  ); // JSX
+};
 
-// 3. Exportera 
-export default App
+// 3. Exportera
+export default App;
