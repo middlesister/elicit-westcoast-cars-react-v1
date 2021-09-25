d20fa9cf7d9639d5e9ac21d2799860754e4d9b3c
Karin Taliga <karin.taliga@elicit.se>
2021-09-25 09:51:52 +0200

    Add styles in JSX - notice it's not html nor css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+if(module.hot) {
+    module.hot.accept();
+}
 // 2. Skapa en React komponent
 const App = () => {
-    return <div>Hello from React</div> // JSX
+    return <div style={{ fontWeight: 'bold', color: 'red' }}>Hello from React!!</div> // JSX
 }
 
 // 3. Visa komponenten i webbläsaren
