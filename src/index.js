c0f6a449264a1b771e4b87b01b1c5c655a5f35d0
Karin Taliga <karin.taliga@elicit.se>
2021-09-25 09:42:28 +0200

    Add a first component and render it

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+// 1. Importera React och ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 2. Skapa en React komponent
+const App = () => {
+    return <div>Hello from React</div> // JSX
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3. Visa komponenten i webbläsaren
+// första argumentet är komponenten, andra är placeringen i DOM
+ReactDOM.render(<App/>, document.querySelector('#root'));
