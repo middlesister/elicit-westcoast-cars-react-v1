64308e09e0b5d31129c28f8bd64664cd13d7b3bf
Karin Taliga <karin.taliga@elicit.se>
2021-09-25 13:55:46 +0200

    Iterate over a list, add a Card and VehicleDetail for each element

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,11 +1,69 @@
 // 1. Importera React
-import React, { useState } from "react";
+import React from "react";
 
 import VehicleDetail from "./VehicleDetail";
 import Card from "./shared/Card";
 
 // 2. Skapa komponenten
 const Vehicles = () => {
+  const vehicles = [
+    {
+      id: 1,
+      registrationNo: "ABC123",
+      make: "Chevrolet",
+      model: "Corvette",
+      modelYear: "2015",
+      imageUrl: "https://i.postimg.cc/N09fbtKZ/car1.jpg",
+      mileage: 25000,
+      description:
+        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam commodo venenatis rhoncus. Vivamus id lacus id elit tristique vulputate. Donec ut diam vitae ligula mollis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras tempor sed elit nec pretium. In accumsan odio id est vehicula tincidunt. Aliquam tincidunt risus nec tellus auctor, eget gravida purus efficitur. Morbi rhoncus erat ac leo gravida, id varius ex rhoncus. Donec a ultricies est. Phasellus congue tincidunt vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus pretium vel odio et tincidunt.Ut dignissim mi ac elit facilisis feugiat. Etiam velit ligula, interdum id nisi sed, auctor tristique felis. Pellentesque laoreet eleifend nibh eu consequat. Aenean ex odio, interdum id purus quis, vulputate consectetur leo. Vestibulum vitae commodo mauris. Vestibulum blandit, tortor vitae aliquam euismod, urna orci condimentum quam, in dapibus urna tortor quis nibh. Maecenas tempus elit nibh, in luctus ipsum dignissim vel. Ut volutpat enim et est aliquam rhoncus.",
+    },
+    {
+      id: 2,
+      registrationNo: "AAA123",
+      make: "Ford",
+      model: "Mustang",
+      modelYear: "2017",
+      imageUrl: "https://i.postimg.cc/Fsy2yyh8/car2.jpg",
+      mileage: 48500,
+      description:
+        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam commodo venenatis rhoncus. Vivamus id lacus id elit tristique vulputate. Donec ut diam vitae ligula mollis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras tempor sed elit nec pretium. In accumsan odio id est vehicula tincidunt. Aliquam tincidunt risus nec tellus auctor, eget gravida purus efficitur. Morbi rhoncus erat ac leo gravida, id varius ex rhoncus. Donec a ultricies est. Phasellus congue tincidunt vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus pretium vel odio et tincidunt.Ut dignissim mi ac elit facilisis feugiat. Etiam velit ligula, interdum id nisi sed, auctor tristique felis. Pellentesque laoreet eleifend nibh eu consequat. Aenean ex odio, interdum id purus quis, vulputate consectetur leo. Vestibulum vitae commodo mauris. Vestibulum blandit, tortor vitae aliquam euismod, urna orci condimentum quam, in dapibus urna tortor quis nibh. Maecenas tempus elit nibh, in luctus ipsum dignissim vel. Ut volutpat enim et est aliquam rhoncus.",
+    },
+    {
+      id: 3,
+      registrationNo: "BBB123",
+      make: "Porsche",
+      model: "Alpine",
+      modelYear: "1967",
+      imageUrl: "https://i.postimg.cc/85xqHwJQ/car3.jpg",
+      mileage: 89000,
+      description:
+        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam commodo venenatis rhoncus. Vivamus id lacus id elit tristique vulputate. Donec ut diam vitae ligula mollis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras tempor sed elit nec pretium. In accumsan odio id est vehicula tincidunt. Aliquam tincidunt risus nec tellus auctor, eget gravida purus efficitur. Morbi rhoncus erat ac leo gravida, id varius ex rhoncus. Donec a ultricies est. Phasellus congue tincidunt vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus pretium vel odio et tincidunt.Ut dignissim mi ac elit facilisis feugiat. Etiam velit ligula, interdum id nisi sed, auctor tristique felis. Pellentesque laoreet eleifend nibh eu consequat. Aenean ex odio, interdum id purus quis, vulputate consectetur leo. Vestibulum vitae commodo mauris. Vestibulum blandit, tortor vitae aliquam euismod, urna orci condimentum quam, in dapibus urna tortor quis nibh. Maecenas tempus elit nibh, in luctus ipsum dignissim vel. Ut volutpat enim et est aliquam rhoncus.",
+    },
+    {
+      id: 4,
+      registrationNo: "CCC123",
+      make: "Mercedes",
+      model: "S500",
+      modelYear: "2019",
+      imageUrl: "https://i.postimg.cc/MHRgrRVc/car4.jpg",
+      mileage: 25000,
+      description:
+        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam commodo venenatis rhoncus. Vivamus id lacus id elit tristique vulputate. Donec ut diam vitae ligula mollis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras tempor sed elit nec pretium. In accumsan odio id est vehicula tincidunt. Aliquam tincidunt risus nec tellus auctor, eget gravida purus efficitur. Morbi rhoncus erat ac leo gravida, id varius ex rhoncus. Donec a ultricies est. Phasellus congue tincidunt vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus pretium vel odio et tincidunt.Ut dignissim mi ac elit facilisis feugiat. Etiam velit ligula, interdum id nisi sed, auctor tristique felis. Pellentesque laoreet eleifend nibh eu consequat. Aenean ex odio, interdum id purus quis, vulputate consectetur leo. Vestibulum vitae commodo mauris. Vestibulum blandit, tortor vitae aliquam euismod, urna orci condimentum quam, in dapibus urna tortor quis nibh. Maecenas tempus elit nibh, in luctus ipsum dignissim vel. Ut volutpat enim et est aliquam rhoncus.",
+    },
+    {
+      id: 5,
+      registrationNo: "XYZ123",
+      make: "Mercedes",
+      model: "AMG",
+      modelYear: "2014",
+      imageUrl: "https://i.postimg.cc/wj9n01vv/car5.jpg",
+      mileage: 25000,
+      description:
+        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam commodo venenatis rhoncus. Vivamus id lacus id elit tristique vulputate. Donec ut diam vitae ligula mollis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras tempor sed elit nec pretium. In accumsan odio id est vehicula tincidunt. Aliquam tincidunt risus nec tellus auctor, eget gravida purus efficitur. Morbi rhoncus erat ac leo gravida, id varius ex rhoncus. Donec a ultricies est. Phasellus congue tincidunt vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus pretium vel odio et tincidunt.Ut dignissim mi ac elit facilisis feugiat. Etiam velit ligula, interdum id nisi sed, auctor tristique felis. Pellentesque laoreet eleifend nibh eu consequat. Aenean ex odio, interdum id purus quis, vulputate consectetur leo. Vestibulum vitae commodo mauris. Vestibulum blandit, tortor vitae aliquam euismod, urna orci condimentum quam, in dapibus urna tortor quis nibh. Maecenas tempus elit nibh, in luctus ipsum dignissim vel. Ut volutpat enim et est aliquam rhoncus.",
+    },
+  ];
+
   return (
     <div>
       <section className="vehicle-form mb-6">
@@ -19,35 +77,24 @@ const Vehicles = () => {
           <div className="mb-3">
             <input type="text" placeholder="Modell" />
           </div>
-          <div className="vehicle-form-two-button">
-            <button type="submit" className="btn btn-outline-rounded">
-              Spara
-            </button>
-            <button className="btn btn-outline-rounded">Avbryt</button>
-          </div>
         </form>
       </section>
       <section>
-        <Card>
-          <VehicleDetail
-            registrationNo="CER345"
-            imageUrl="https://i.postimg.cc/N09fbtKZ/car1.jpg"
-            make="Chevreolet"
-            model="Corvette"
-            modelYear="2019"
-            mileage="12000"
-          />
-        </Card>
-        <Card>
-          <VehicleDetail
-            registrationNo="ABC123"
-            imageUrl="https://i.postimg.cc/Fsy2yyh8/car2.jpg"
-            make="Ford"
-            model="Mustang"
-            modelYear="1978"
-            mileage="28900"
-          />
-        </Card>
+        {/* Create a new Card and VehicleDetail for each item in our list */}
+        {vehicles.map((vehicle) => {
+          return (
+            <Card>
+              <VehicleDetail
+                registrationNo={vehicle.registrationNo}
+                imageUrl={vehicle.imageUrl}
+                make={vehicle.make}
+                model={vehicle.model}
+                modelYear={vehicle.modelYear}
+                mileage={vehicle.mileage}
+              />
+            </Card>
+          );
+        })}
       </section>
     </div>
   );
